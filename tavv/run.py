7bff28523b61a832fd04dcbe4804c8c8dff3d183
Kenichi Chiba <kechiba@ncc.go.jp>
2018-08-14 03:07:12 +0000

    modify run.py

--- tavv/run.py
+++ tavv/run.py
@@ -16,18 +16,15 @@ def subprocess_exec(args,toolname,commands):
     stderr_f.close()
 
 def validator_main(args):
-    # commands = ["vcf_validator_linux", "-i", args.input_vcf + "-o", args.output]
-    commands = ["vcf_validator", "-i", args.input_vcf, "-o", args.output]
+    commands = ["vcf_validator_linux", "-i", args.input_vcf + "-o", args.output]
     subprocess_exec(args,"vcf_validator",commands)
 
 def vcfcheck_main(args):
-    # commands = ["vcfcheck", "-f", args.reference, args.input_vcf]
-    commands = ["/home/ec2-user/environment/tools/vcflib/bin/vcfcheck", "-f", args.reference, args.input_vcf]
+    commands = ["vcfcheck", "-f", args.reference, args.input_vcf]
     subprocess_exec(args,"vcflib-vcfcheck",commands)
 
 def gatk_main(args):
-    # commands = ["gatk","ValidateVariants","-R", args.reference, "-V", args.input_vcf, "--validation-type-to-exclude","ALLELES"]
-    commands = ["/home/ec2-user/environment/tools/gatk-4.0.7.0/gatk","ValidateVariants", "-R", args.reference, "-V", args.input_vcf, "--validation-type-to-exclude ALLELES"]
+    commands = ["gatk","ValidateVariants","-R", args.reference, "-V", args.input_vcf, "--validation-type-to-exclude","ALLELES"]
     subprocess_exec(args,"gatk-validatevariants",commands)
 
 def all_main(args):
