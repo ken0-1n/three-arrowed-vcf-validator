44614cc12a489f0c13f204815e44973927faaed1
Kenichi Chiba <kechiba@ncc.go.jp>
2018-08-14 01:06:29 +0000

    modify run.py

--- tavv/run.py
+++ tavv/run.py
@@ -6,7 +6,8 @@ from datetime import datetime
 def validator_main(args):
 
     dt = datetime.now().strftime("%Y%m%d%H%M%S")
-    os.mkdir(args.output)
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
     basename = os.path.basename(args.input_vcf)
 
     print "-------vcf_validator--------"
@@ -28,7 +29,7 @@ def validator_main(args):
     stderr_f.close()
 
     print "-------gatk ValidateVariants--------"
-    commands = ["gatk", "-R", args.reference, "-V", args.input_vcf, "--validation-type-to-exclude ALLELES"]
+    commands = ["gatk","ValidateVariants","-R", args.reference, "-V", args.input_vcf, "--validation-type-to-exclude ALLELES"]
     # commands = ["/home/ec2-user/environment/tools/gatk-4.0.7.0/gatk", "-R", args.reference, "-V", args.input_vcf, "--validation-type-to-exclude ALLELES"]
     stdout_f = open(args.output +"/"+basename+".gatk-validatevariants.stdout."+dt+".txt", 'w')
     stderr_f = open(args.output +"/"+basename+".gatk-validatevariants.stderr."+dt+".txt", 'w')
